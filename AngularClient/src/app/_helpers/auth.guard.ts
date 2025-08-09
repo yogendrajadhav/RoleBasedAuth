c25bd8f60968428a9d65bef025424fe313a0b0e3
yogendra jadhav <yj.withyou@gmail.com>
2025-08-09 18:22:53 +0530

    canActivate interface implemented to check if user has ADMIN role

--- AngularClient/src/app/_helpers/auth.guard.ts
+++ AngularClient/src/app/_helpers/auth.guard.ts
@@ -1,8 +1,26 @@
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from '../_services/authentication.service';
 
+@Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate{
+  
+  constructor(private authService:AuthenticationService, private router:Router){}
   canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
-    
-    return true;
+    const user=this.authService.userValue;
+    if(user){
+      // check if route is restricted by role
+      const {roles}=route.data;
+      if(roles && !roles.includes(user.role)){
+        // role not authorized so redirect to home page
+        this.router.navigate(['/']);
+        return false;
+      }
+      // authorized so return true  
+      return true;
+    }
+     // not logged in so redirect to login page with the return url
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
   }
 }
