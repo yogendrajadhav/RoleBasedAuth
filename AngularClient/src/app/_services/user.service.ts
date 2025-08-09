d8325aeb4a4e093310718acb28393ec66483d1fd
yogendra jadhav <yj.withyou@gmail.com>
2025-08-09 17:07:11 +0530

    user service getAll and getById functions implemented

--- AngularClient/src/app/_services/user.service.ts
+++ AngularClient/src/app/_services/user.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../_models/user';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+ constructor(private http: HttpClient) { }
 
-  constructor() { }
+    getAll() {
+        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+    }
+
+    getById(id: number) {
+        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+    }
 }
