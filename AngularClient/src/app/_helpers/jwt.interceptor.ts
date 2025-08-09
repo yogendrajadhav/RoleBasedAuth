7692742d7336a59a5596a7288b3cfc4e97b4686f
yogendra jadhav <yj.withyou@gmail.com>
2025-08-09 18:19:17 +0530

    @Injectable decorator added to fix null provider issue

--- AngularClient/src/app/_helpers/jwt.interceptor.ts
+++ AngularClient/src/app/_helpers/jwt.interceptor.ts
@@ -1,8 +1,16 @@
+import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../_services/authentication.service';
 import { environment } from '../../environments/environment';
 
+/**
+ * JwtInterceptor is an HTTP interceptor that adds a JWT token to outgoing HTTP requests
+ * if the user is logged in and the request is to the API URL.
+ */
+@Injectable({
+  providedIn: 'root'
+})
 export class JwtInterceptor implements HttpInterceptor {
   constructor(private authService: AuthenticationService) {}
   
