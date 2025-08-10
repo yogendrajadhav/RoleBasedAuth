96b811f7f0f77b2b33eaf05c82c3717fdb426d6a
yogendra jadhav <yj.withyou@gmail.com>
2025-08-10 13:40:24 +0530

    URL path fixed

--- AngularClient/src/environments/environment.development.ts
+++ AngularClient/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000/api',
-    authUrl: 'http://localhost:3000/auth'
+     authUrl: 'https://localhost:5000/api/account'
 };
