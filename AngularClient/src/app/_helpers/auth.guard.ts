c2891228d4c9a4f1b6c20cb395e9c7250e45227d
yogendra jadhav <yj.withyou@gmail.com>
2025-08-10 23:11:30 +0530

    token expiry check implemented to remove expired token

--- AngularClient/src/app/_helpers/auth.guard.ts
+++ AngularClient/src/app/_helpers/auth.guard.ts
@@ -1,41 +1,48 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationService } from '../_services/authentication.service';
+import { ToastrService } from 'ngx-toastr';
 
-@Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate{
-  
-  constructor(private authService:AuthenticationService, private router:Router){}
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
 
-  canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
-    
-    if (this.authService.isTokenExpired()) {
-      this.authService.removeExpiredToken(); // or remove only specific keys
-      this.router.navigate(['/login']);
-      return false;
-    }
+  constructor(private authService: AuthenticationService, private router: Router, private toastr: ToastrService) { }
 
-    const rolesAssigned=this.authService.getUserRoles();
-    if(rolesAssigned && rolesAssigned.length>0){
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+
+
+
+    const rolesAssigned = this.authService.getUserRoles();
+
+    if (rolesAssigned && rolesAssigned.length > 0) {
       // check if route is restricted by role
-    const {roles} = route.data;
-    if (roles && Array.isArray(roles)) {
-      const hasRole = roles.some((role: string) => rolesAssigned.includes(role));
-      if (hasRole) {
-        // authorized so return true
-        return true;
+      const { roles } = route.data;
+      if (roles && Array.isArray(roles)) {
+        const hasRole = roles.some((role: string) => rolesAssigned.includes(role));
+        if (hasRole) {
+          // authorized so return true
+          return true;
+        } else {
+          // role not authorized so redirect to home page
+          this.router.navigate(['/']);
+          return false;
+        }
       } else {
-        // role not authorized so redirect to home page
-        this.router.navigate(['/']);
-        return false;
+        
+        if (this.authService.isTokenExpired()) {
+          this.toastr.warning("Token expired. Redirecting to login.")
+          this.authService.removeExpiredToken(); // or remove only specific keys
+          this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+          return false;
+        }
+
+        // if no roles are defined on the route, allow access
+        return true;
       }
-    } else {
-      // if no roles are defined on the route, allow access
-      return true;
+
     }
-      
-    }
-     // not logged in so redirect to login page with the return url
+
+    // not logged in so redirect to login page with the return url
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     return false;
   }
