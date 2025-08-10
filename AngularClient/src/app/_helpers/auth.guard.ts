68e691ea99bbc5f6be62791700a1208f4900756d
yogendra jadhav <yj.withyou@gmail.com>
2025-08-10 13:35:27 +0530

    fixed - role comparison

--- AngularClient/src/app/_helpers/auth.guard.ts
+++ AngularClient/src/app/_helpers/auth.guard.ts
@@ -7,17 +7,25 @@ export class AuthGuard implements CanActivate{
   
   constructor(private authService:AuthenticationService, private router:Router){}
   canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
-    const user=this.authService.userValue;
-    if(user){
+    const rolesAssigned=this.authService.getUserRoles();
+    if(rolesAssigned && rolesAssigned.length>0){
       // check if route is restricted by role
-      const {roles}=route.data;
-      if(roles && !roles.includes(user.role)){
+    const {roles} = route.data;
+    if (roles && Array.isArray(roles)) {
+      const hasRole = roles.some((role: string) => rolesAssigned.includes(role));
+      if (hasRole) {
+        // authorized so return true
+        return true;
+      } else {
         // role not authorized so redirect to home page
         this.router.navigate(['/']);
         return false;
       }
-      // authorized so return true  
+    } else {
+      // if no roles are defined on the route, allow access
       return true;
+    }
+      
     }
      // not logged in so redirect to login page with the return url
     this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
