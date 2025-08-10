2e5ebf498e7a240b8a668ac7a749f5e28e258971
yogendra jadhav <yj.withyou@gmail.com>
2025-08-10 23:09:55 +0530

    more fields added

--- AngularClient/src/app/_models/user.ts
+++ AngularClient/src/app/_models/user.ts
@@ -1,10 +1,12 @@
 import { Role } from "./role";
 
 export interface User {
-    id:number;
-    firstName: string;
-    lastName: string;
+    id?:number;
+    firstName?: string;
+    lastName?: string;
     userName: string;
+    email?: string;
     role: Role;
     token?: string;
+    profilePicture?:string
 }
